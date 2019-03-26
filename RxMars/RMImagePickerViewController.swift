51ed28ac5746b8c5b9b26c2f61e4b49db7a233dd
aipinn <allenpengplay@gmail.com>
2019-03-26 21:00:46 +0800

    add LLVM node

--- RxMars/RMImagePickerViewController.swift
+++ RxMars/RMImagePickerViewController.swift
@@ -38,9 +38,39 @@ class RMImagePickerViewController: UIViewController {
             .bind(to: imageView.rx.image)
             .disposed(by: disposeBag)
         
-      
+        galleryButton.rx.tap
+            .flatMapLatest { [weak self] _ in
+                return UIImagePickerController.rx.createWithParent(self) { picker in
+                    picker.sourceType = .photoLibrary
+                    picker.allowsEditing = false
+                    }
+                    .flatMap {
+                        $0.rx.didFinishPickingMediaWithInfo
+                    }
+                    .take(1)
+            }
+            .map { info in
+                return info["UIImagePickerControllerOriginalImage"] as? UIImage
+            }
+            .bind(to: imageView.rx.image)
+            .disposed(by: disposeBag)
+        
+        cropButton.rx.tap
+            .flatMapLatest { [weak self] _ in
+                return UIImagePickerController.rx.createWithParent(self) { picker in
+                    picker.sourceType = .photoLibrary
+                    picker.allowsEditing = true
+                    }
+                    .flatMap { $0.rx.didFinishPickingMediaWithInfo }
+                    .take(1)
+            }
+            .map { info in
+                return info["UIImagePickerControllerEditedImage"] as? UIImage
+            }
+            .bind(to: imageView.rx.image)
+            .disposed(by: disposeBag)
+        
     }
-
     
 
 }
