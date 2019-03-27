ac86201c663028e69fd7f254237b97493d0f5d3a
aipinn <allenpengplay@gmail.com>
2019-03-28 00:28:43 +0800

    add location

--- RxMars/RMGeolocationService.swift
+++ RxMars/RMGeolocationService.swift
@@ -11,8 +11,47 @@ import RxCocoa
 import RxSwift
 
 class GeolocationService {
-    
+    //单例
     static let instance = GeolocationService()
+    //Driver: 不会产生错误 主线程 共享状态变化
     private (set) var authorized: Driver<Bool>
+    private (set) var location: Driver<CLLocationCoordinate2D>
     
+    private let locationManager = CLLocationManager()
+    
+    private init() {
+        locationManager.distanceFilter = kCLDistanceFilterNone
+        locationManager.desiredAccuracy = kCLLocationAccuracyBestForNavigation
+        
+        authorized = Observable.deferred { [weak locationManager] in
+            let status = CLLocationManager.authorizationStatus()
+            guard let locationManager = locationManager else {
+                return Observable.just(status)
+            }
+            return locationManager
+                .rx
+                .didChangeAuthorizationStatus
+                .startWith(status)
+        }
+        .asDriver(onErrorJustReturn: CLAuthorizationStatus.notDetermined)
+            .map {
+                switch $0 {
+                case .authorizedAlways:
+                    return false
+                default:
+                    return true
+                }
+        }
+        location = locationManager
+            .rx.didUpdateLocations
+            .asDriver(onErrorJustReturn:[])
+            .flatMap {
+                return $0.last.map(Driver.just) ?? Driver.empty()
+            }.map { $0.coordinate }
+        
+        locationManager.requestAlwaysAuthorization()
+        locationManager.startUpdatingLocation()
+        
+        
+    }
 }
--- RxMars/RMLiteViewController.swift
+++ RxMars/RMLiteViewController.swift
@@ -9,6 +9,15 @@
 import UIKit
 import RxCocoa
 import RxSwift
+import CoreLocation
+
+private extension Reactive where Base: UILabel {
+    var coordinate: Binder<CLLocationCoordinate2D> {
+        return Binder(base) { label, location in
+            label.text = "lat: \(location.latitude) \nLon: \(location.longitude)"
+        }
+    }
+}
 
 class RMLiteViewController: UIViewController {
 
@@ -19,11 +28,16 @@ class RMLiteViewController: UIViewController {
     @IBOutlet weak var sumL: UILabel!
     
     
+    @IBOutlet weak var statusL: UILabel!//地理位置是否可用
+    @IBOutlet weak var infoL: UILabel!//地理位置经纬度信息
+    @IBOutlet weak var openBtn1: UIButton!//打开偏好设置
+    @IBOutlet weak var descL: UILabel!//地理位置打开关闭描述信息
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         simpleAddition()
+        location()
     }
     //一个简单的加法
     func simpleAddition() {
@@ -38,4 +52,42 @@ class RMLiteViewController: UIViewController {
             .disposed(by: disposeBag)
     }
 
+    func location() {
+        let noGeolocationView = UIView(frame: view.bounds)
+        noGeolocationView.alpha = 0.6;
+        noGeolocationView.backgroundColor = .white
+        let btn = UIButton(frame: CGRect(x: 10, y: 200, width: 100, height: 30))
+        btn.setTitle("Open preference", for: .normal)
+        btn.backgroundColor = .orange
+        noGeolocationView.addSubview(btn)
+        view.addSubview(noGeolocationView)
+        
+        let geolocationService = GeolocationService.instance
+        
+        geolocationService.authorized
+        .drive(noGeolocationView.rx.isHidden)
+        .disposed(by: disposeBag)
+        
+        geolocationService.location
+        .drive(infoL.rx.coordinate)
+        .disposed(by: disposeBag)
+        
+        openBtn1.rx.tap
+            .bind { [weak self] _ -> Void in
+                self?.openAppPreferences()
+        }
+        .disposed(by: disposeBag)
+        
+        btn.rx.tap
+            .bind { [weak self] _ ->Void in
+                    self?.openAppPreferences()
+        }
+        .disposed(by: disposeBag)
+    }
+    
+    private func openAppPreferences() {
+        UIApplication.shared.openURL(URL(string: UIApplication.openSettingsURLString)!)
+        
+        //UIApplication.shared.open(URL(string: UIAlertAction.openSettingsURLString)!, options: [], completionHandler: nil)
+    }
 }
